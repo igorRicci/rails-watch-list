673e0f98f920f73e8683ce8dae860cf58a2a1f0c
igor ricci <assis.ricci@gmail.com>
2022-05-26 16:34:13 -0300

    added beautiful banner

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'lists#index'
+  # root to: 'lists#index'
+  root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
