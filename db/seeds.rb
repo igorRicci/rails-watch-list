c82e805446a18a31f04133497709b0ef4315db5c
igor ricci <assis.ricci@gmail.com>
2022-05-31 09:45:29 -0300

    added env key to tmdb api

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,12 @@ Movie.create!(title: "The Shawshank Redemption", overview: "Framed in the 1940s 
 Movie.create!(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create!(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
+url = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_KEY']}&language=en-US&page="
+
+poster_base_url = 'https://image.tmdb.org/t/p/w500'
+
+puts ' Generating movies '.center(50, '=')
+
 URI.open("http://tmdb.lewagon.com/movie/top_rated") do |lines|
   lines.each_line do |line|
     results = JSON.parse(line)
